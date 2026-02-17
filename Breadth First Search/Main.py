c26b49cbb7abf5cdb72d10348a949a181bb58358
Who cares <Thakurtejas567@gmail.com>
2026-02-17 15:34:41 +0530

    Update Main.py

--- Breadth First Search/Main.py
+++ Breadth First Search/Main.py
@@ -1,1 +1,28 @@
+from collections import deque
+
+def bfs(graph, start_Node):
+    visited = set()
+    queue = deque([start_Node])
+    visited.add(start_Node)
+
+    print(f"Starting BFS traversal from the node: {start_Node}")
+    while queue:
+        current_Node = queue.popleft()
+        print(current_ Node, end=" ")
+
+        for Neighbor in graph[current_ Node]:
+            if Neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+sample_graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+bfs(sample_graph, 'A')
 
